845e8bc444ba05b005be2f9bb4bd1baf62a1d495
MuhammadA-C <phantomganhdi@gmail.com>
2023-09-26 15:19:16 -0700

    Javadocs Pt3
    completed the javadeocs for the model folder.

--- muhammad.chambers.c195.pa/src/main/java/com/example/muhammad/chambers/c195/pa/model/ReportOne.java
+++ muhammad.chambers.c195.pa/src/main/java/com/example/muhammad/chambers/c195/pa/model/ReportOne.java
@@ -8,13 +8,21 @@ import java.sql.SQLException;
 
 /** This class is used to create a ReportOne object.*/
 public class ReportOne {
+    /** Holds the type of appointment*/
     private String type;
+    /** Holds the month*/
     private String month;
+    /** Holds the total number of appointments*/
     private int totalNumber;
+    /** List that holds all report objects*/
     private static ObservableList<ReportOne> reports = FXCollections.observableArrayList();
 
 
-    //Constructor
+    /** This is the Constructor for the ReportOne class.
+     This Constructor sets all the fields for the ReportOne object.
+     @param type the type to set
+     @param month the month to set
+     @param totalNumber the totalNumber to set*/
     public ReportOne(String type, String month, int totalNumber) {
         this.setType(type);
         this.setMonth(month);
@@ -22,19 +30,30 @@ public class ReportOne {
     }
 
 
-    //Getter methods
+    /** This is the getType method.
+     This method returns the type field.
+     @return Returns the type*/
     public String getType() {
         return this.type;
     }
 
+    /** This is the getMonth method.
+     This method returns the month field.
+     @return Returns the month*/
     public String getMonth() {
         return this.month;
     }
 
+    /** This is the getTotalNumber method.
+     This method returns the totalNumber field.
+     @return Returns the totalNumber*/
     public int getTotalNumber() {
         return this.totalNumber;
     }
 
+    /** This is the getReportList method.
+     This method returns the reports list field.
+     @return Returns the reports*/
     public static ObservableList<ReportOne> getReportsList() throws SQLException {
         reports.clear();
         populateReportsList();
@@ -42,20 +61,30 @@ public class ReportOne {
         return reports;
     }
 
-
-    //Setter methods
+    /** This is the setType method.
+     This method sets the type field.
+     @param type the type to set*/
     public void setType(String type) {
         this.type = type.toLowerCase();
     }
 
+    /** This is the setMonth method.
+     This method sets the month field.
+     @param month the month to set*/
     public void setMonth(String month) {
         this.month = month.toLowerCase();
     }
 
+    /** This is the setTotalNumber method.
+     This method sets the totalNumber field.
+     @param totalNumber the totalNumber to set*/
     public void setTotalNumber(int totalNumber) {
         this.totalNumber = totalNumber;
     }
 
+    /** This is the addReportToReportsList method.
+     This method adds a reportOne object to the reports list.
+     @param reportOne the reportOne to add*/
     private static void addReportToReportsList(ReportOne reportOne) {
         if(reports.size() == 0) {
             reports.add(reportOne);
@@ -77,8 +106,8 @@ public class ReportOne {
         reports.add(reportOne);
     }
 
-
-    //Methods
+    /** This is the populateReportsList method.
+     This method pulls the appointment data from the database, created report one objects, and adds the objects to the reports list.*/
     private static void populateReportsList() throws SQLException {
         for(Appointment appointment : AppointmentDAOImpl.getAppointmentsList()) {
             String month = appointment.getStart().toLocalDateTime().toLocalDate().getMonth().toString();
