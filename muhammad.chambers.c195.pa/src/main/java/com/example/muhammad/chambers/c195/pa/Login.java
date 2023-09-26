c61793a93cc039ee6acdb8286347d2b1729a9971
MuhammadA-C <phantomganhdi@gmail.com>
2023-09-26 14:20:53 -0700

    JavaDocs Pt1
    Working on completing the javadocs.

--- muhammad.chambers.c195.pa/src/main/java/com/example/muhammad/chambers/c195/pa/Login.java
+++ muhammad.chambers.c195.pa/src/main/java/com/example/muhammad/chambers/c195/pa/Login.java
@@ -2,24 +2,29 @@ package com.example.muhammad.chambers.c195.pa;
 
 import com.example.muhammad.chambers.c195.pa.dao.*;
 import com.example.muhammad.chambers.c195.pa.helper.*;
-import com.example.muhammad.chambers.c195.pa.model.*;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/*
+     Muhammad Chambers
+     Western Governors University
+     C195 Software 2
+     Term 2
+     26 September 2023
+ */
+
+/** This class creates the application which allows you to create appointments, customer records, and view reports*/
 public class Login extends Application {
+
+    /** This is the start method.
+     This method creates the Main Menu Screen when the program starts.
+     @param stage the stage to set*/
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login-view.fxml"));
@@ -29,6 +34,9 @@ public class Login extends Application {
         stage.show();
     }
 
+    /** This is the main method.
+     This method is where the program starts and executes code.
+     @param args the args to use*/
     public static void main(String[] args) throws SQLException {
         JDBC.openConnection();
         //Locale.setDefault(new Locale("fr")); //Changes the language for the software
